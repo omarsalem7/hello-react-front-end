280c4f56eb859fde58d262e75065b3140255eaba
omarsalem7 <omarsalem0721@gmail.com>
2022-06-08 11:40:48 +0200

    Fix linters

--- src/API/messagesApi.js
+++ src/API/messagesApi.js
@@ -1,8 +1,6 @@
 import axios from 'axios';
 
 const fetchMessagesAPI = () =>
-  axios
-    .get('http://localhost:3000/api/messages')
-    .then((response) => response.data.message);
+  axios.get('http://localhost:3000/api/messages').then((response) => response.data.message);
 
 export default fetchMessagesAPI;
